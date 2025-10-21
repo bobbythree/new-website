883c2ddfa320a6dc3c859c87bd99f807b155a6aa
bobbythree <bobbythr33@gmail.com>
2025-10-21 18:31:55 -0500

    adjust fontsize

--- src/components/HomepageHero.tsx
+++ src/components/HomepageHero.tsx
@@ -2,7 +2,7 @@ export default function HomepageHero() {
   return (
     <>
       <h1 className="text-7xl">Robert Lewis III</h1>
-      <h3 className="text-5xl">Freelance Software Engineer</h3>
+      <h3 className="text-4xl mt-1">Freelance Software Engineer</h3>
     </>
   )
 }
