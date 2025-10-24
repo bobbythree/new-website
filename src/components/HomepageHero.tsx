ca4b38b85acdbc9224aa1cf530b8cb0212aadb2e
bobbythree <bobbythr33@gmail.com>
2025-10-24 18:01:46 -0500

    additional text to homepage hero

--- src/components/HomepageHero.tsx
+++ src/components/HomepageHero.tsx
@@ -3,6 +3,9 @@ export default function HomepageHero() {
     <>
       <h1 className="text-7xl">Robert Lewis III</h1>
       <h3 className="text-4xl mt-1">Freelance Software Engineer</h3>
+      <p className="text-xl mt-10 w-xl">
+        I am passionate about creating engaging and functional web applications. I love building intuitive user interfaces, robust back-end systems, and bringing unique ideas to life.
+      </p>
     </>
   )
 }
