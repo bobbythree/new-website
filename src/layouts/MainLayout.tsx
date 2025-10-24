5a86aa3578d9712db50f0983ea5e8660146107fe
bobbythree <bobbythr33@gmail.com>
2025-10-24 17:52:48 -0500

    radial gradient background

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,18 +2,27 @@ import { Link, Outlet } from "react-router";
 
 export default function MainLayout() {
   return (
-    <>
-      <nav className="flex justify-between px-25 items-center h-15 bg-sky-100 absolute w-full">
+    <div className="relative min-h-screen">
+
+      <div
+        className="absolute inset-0 z-0 
+                   bg-radial at-center 
+                   from-sky-200 via-cyan-500 to-sky-700"
+      >
+      </div>
+
+      <nav className="flex justify-between px-25 items-center h-15 absolute w-full z-10">
         <h3>LOGO</h3>
         <div className="flex gap-10">
           <Link to="/portfolio">portfolio</Link>
           <Link to="/contact">contact</Link>
         </div>
       </nav>
-      <main className="flex grow h-screen items-center justify-center">
+
+      <main className="relative z-10 flex grow h-screen items-center justify-center pt-15">
         <Outlet />
       </main>
-    </>
+
+    </div>
   )
 }
-
