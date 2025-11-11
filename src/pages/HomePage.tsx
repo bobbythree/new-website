a40871172686f5d50890dc4fd7dfd64cfd96a049
bobbythree <bobbythr33@gmail.com>
2025-11-10 22:17:04 -0600

    make img translucent

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,17 +6,17 @@ export default function HomePage() {
     <div className="relative min-h-screen w-full bg-gray-800 flex items-center justify-center">
       {/* Background Image that shrinks with screen */}
       <div
-        className="absolute top-0 left-0 w-full h-full flex items-center justify-center"
+        className="absolute top-0 left-0 flex items-center justify-center"
       >
         <img
           src={bgImage}
           alt="Background"
-          className="w-full h-auto max-w-[1920px] object-contain"
+          className="w-full h-auto max-w-[1920px] object-contain opacity-50"
         />
       </div>
 
       {/* Optional overlay for text contrast */}
-      <div className="absolute top-0 left-0 w-full h-full bg-black/20"></div>
+      <div className="absolute top-0 left-0 w-full h-full"></div>
 
       {/* Foreground content */}
       <div className="relative z-10 w-full flex flex-col items-center justify-center px-4 text-center">
