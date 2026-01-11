c104c6f456fea153054e008a9970f56d81b86688
bobbythree <bobbythr33@gmail.com>
2026-01-11 17:21:15 -0600

    hero colors

--- src/components/HomepageHero.tsx
+++ src/components/HomepageHero.tsx
@@ -1,10 +1,10 @@
 export default function HomepageHero() {
   return (
     <div className="text-center w-3xl flex flex-col items-center justify-center">
-      <h1 className="text-6xl text-sky-600">Robert Lewis III</h1>
-      <h3 className="text-3xl mt-1 text-sky-600">Freelance Software Engineer</h3>
-      <p className="text-md mt-5 w-md text-sky-100">
-        Passionate about creating custom web applications. I love building intuitive user interfaces, robust back-end systems, and bringing unique ideas to life.
+      <h1 className="text-7xl text-sky-300">Robert Lewis III</h1>
+      <h3 className="text-4xl mt-1 text-sky-400">Freelance Software Engineer</h3>
+      <p className="text-lg mt-2 w-md text-sky-600">
+        Custom Web Applications for small businesses
       </p>
     </div>
   )
