d708a49ef5c89ecfa17f6ba2ee61c404d2bc6fb5
bobbythree <bobbythr33@gmail.com>
2026-01-11 12:39:37 -0600

    update package and WIP homepage

--- src/components/HomepageHero.tsx
+++ src/components/HomepageHero.tsx
@@ -1,10 +1,10 @@
 export default function HomepageHero() {
   return (
-    <div className="text-center w-3xl text-sky-100">
-      <h1 className="text-7xl">Robert Lewis III</h1>
-      <h3 className="text-4xl mt-1">Freelance Software Engineer</h3>
-      <p className="text-xl mt-10">
-        Passionate about creating engaging and functional web applications. I love building intuitive user interfaces, robust back-end systems, and bringing unique ideas to life.
+    <div className="text-center w-3xl flex flex-col items-center justify-center">
+      <h1 className="text-6xl text-sky-600">Robert Lewis III</h1>
+      <h3 className="text-3xl mt-1 text-sky-600">Freelance Software Engineer</h3>
+      <p className="text-md mt-5 w-md text-sky-100">
+        Passionate about creating custom web applications. I love building intuitive user interfaces, robust back-end systems, and bringing unique ideas to life.
       </p>
     </div>
   )
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,28 +1,13 @@
 import HomepageHero from "../components/HomepageHero";
-import bgImage from "../assets/blue-green.jpg";
+
 
 export default function HomePage() {
   return (
-    <div className="relative min-h-screen w-full bg-gray-800 flex items-center justify-center">
-      {/* Background Image that shrinks with screen */}
-      <div
-        className="absolute top-0 left-0 flex items-center justify-center"
-      >
-        <img
-          src={bgImage}
-          alt="Background"
-          className="w-full h-auto max-w-[1920px] object-contain opacity-50"
-        />
-      </div>
 
-      {/* Optional overlay for text contrast */}
-      <div className="absolute top-0 left-0 w-full h-full"></div>
-
-      {/* Foreground content */}
-      <div className="relative z-10 w-full flex flex-col items-center justify-center px-4 text-center">
-        <HomepageHero />
-      </div>
+    <div className="flex items-center justify-center h-screen bg-zinc-900">
+      <HomepageHero />
     </div>
-  );
+  )
 }
+
 
